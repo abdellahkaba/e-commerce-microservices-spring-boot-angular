a8a98fa6277592dd5700ca52f370259b597abbf7
Abdellah <abdallahkaba98@gmail.com>
2024-09-24 21:48:23 +0000

    Implement the order line controller

--- services/order/src/main/java/com/isi/ecommerce/orderLine/OrderLineMapper.java
+++ services/order/src/main/java/com/isi/ecommerce/orderLine/OrderLineMapper.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderLineMapper {
+
     public OrderLine toOrderLine(OrderLineRequest request) {
         return OrderLine.builder()
                 .id(request.id())
@@ -17,5 +18,11 @@ public class OrderLineMapper {
                 )
                 .productId(request.productId())
                 .build();
+    }
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(
+                orderLine.getId(),
+                orderLine.getQuantity()
+        );
     }
 }
---
+++ services/order/src/main/java/com/isi/ecommerce/orderLine/OrderLineResponse.java
@@ -0,0 +1,6 @@
+package com.isi.ecommerce.orderLine;
+
+public record OrderLineResponse(
+        Integer id,
+        double quantity
+) { }
